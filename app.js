31ac56a2388c53929eed12024a95d26dda3f2bbf
mdimarco <mhd894@gmail.com>
2016-07-29 21:07:29 -0400

    Added twitter api key functionality

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+
+var twitterAPI = require('node-twitter-api');
+var twitter = new twitterAPI({
+	consumerKey: process.env.TWITTER_CONSUMER_KEY,
+	consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+	callback: ''
+});
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // viewed at http://localhost:8080
